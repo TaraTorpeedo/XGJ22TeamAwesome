ab3686e5380ef068b7b6e6be64b151a57f857018
MrBabuls <noora.kar@hotmail.fi>
2022-11-26 16:20:16 +0200

    Commit minigame

---
+++ Assets/CommitEvent.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CommitEvent : MonoBehaviour
+{
+    public GameObject pushButton;
+    public GameObject fetchButton;
+    public void Commit()
+    {
+        pushButton.SetActive(true);
+    }
+    public void FetchCommit()
+    {
+
+    }
+    public void PushCommit()
+    {
+
+    }
+}
