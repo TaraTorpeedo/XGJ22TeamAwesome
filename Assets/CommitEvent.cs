bf232e26a8689b9df396c7bbe7771f634eb07015
MrBabuls <noora.kar@hotmail.fi>
2022-11-26 18:50:07 +0200

    Git game feature

--- Assets/CommitEvent.cs
+++ Assets/CommitEvent.cs
@@ -2,21 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using static Unity.Burst.Intrinsics.X86.Avx;
 
 public class CommitEvent : MonoBehaviour
 {
     public GameObject pushButton;
-    public GameObject fetchButton;
-    public void Commit()
+    public GameObject commitButton;
+    public GameObject fetchButtonUI;
+    public GameObject pushButtonUI;
+    public TMP_InputField commitInput;
+    public void GitButtonsEvent()
     {
-        pushButton.SetActive(true);
-    }
-    public void FetchCommit()
-    {
-
-    }
-    public void PushCommit()
-    {
-
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.name == commitButton.name)
+            {
+                pushButtonUI.SetActive(true);
+                pushButton.SetActive(true);
+                fetchButtonUI.SetActive(false);
+                commitInput.text = string.Empty;
+            }
+            if (hit.transform.name == pushButton.name)
+            {
+                //taski complite
+            }
+        }
     }
 }
